74af9fc5af2aebd98760890eb4ac143e6dcab2e7
Jacob Wong <jacobsunsetbluff@gmail.com>
2025-01-05 17:31:36 -0800

    Implement creative tab bismuth_items and bismuth_blocks tab. Followed Kaupenjoe episode 4

--- src/main/java/com/jwong/mythos/Mythos.java
+++ src/main/java/com/jwong/mythos/Mythos.java
@@ -1,6 +1,7 @@
 package com.jwong.mythos;
 
 import com.jwong.mythos.block.ModBlocks;
+import com.jwong.mythos.item.ModCreativeModeTabs;
 import com.jwong.mythos.item.ModItems;
 import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
@@ -41,6 +42,8 @@ public class Mythos
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
+        ModCreativeModeTabs.register((modEventBus));
 
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
